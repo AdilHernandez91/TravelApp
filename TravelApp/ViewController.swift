5dab1e81bbb5819394800ff148d2bdf19c1d9d95
German Hernandez <adil.hernandez91@gmail.com>
2019-03-29 19:35:19 +0100

    Initial Commit

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by German Hernandez on 29/03/2019.
+//  Copyright © 2019 German Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
